b12928c21d65da0e3b67a30792b11adb0b6deecd
syeed111 <syeedimtiaz111@gmail.com>
2025-08-07 23:59:08 +0600

    auth-controller added

---
+++ backend/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    cartItems: [
+      {
+        quantity: { type: Number, required: true },
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      },
+    ],
+    role: { type: String, enum: ["customer", "admin"], default: "customer" },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
